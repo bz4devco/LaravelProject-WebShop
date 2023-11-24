492839fd492bcd3679596dd679365aac6d9d169b
bz4devco <alibabazadeh12y@gmail.com>
2023-11-24 22:32:58 +0330

    create route for admin part of dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +12,42 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+/*
+|--------------------------------------------------------------------------
+| Admin
+|--------------------------------------------------------------------------
+*/
+
+Route::prefix('admin')->namespace('Admin')->group(function (){
+
+
+    // dashboard route
+    Route::get('/', 'AdminDashboardController@index')->name('admin.home');
+
+    // section market in admin panel
+    Route::prefix('market')->namespace('Market')->group(function (){
+
+        // section market and category side in admin panel
+        Route::prefix('category')->group(function (){
+            Route::get('/', 'CategoryController@index')->name('admin.market.category.index');
+            Route::get('/create', 'CategoryController@create')->name('admin.market.category.create');
+            Route::post('/store', 'CategoryController@store')->name('admin.market.category.store');
+            Route::get('/edit/{id}', 'CategoryController@edit')->name('admin.market.category.edit');
+            Route::put('/update/{id}', 'CategoryController@update')->name('admin.market.category.update');
+            Route::delete('/delete/{id}', 'CategoryController@destroy')->name('admin.market.category.destroy');
+        });
+
+    
+        // section market and brand side in admin panel
+        Route::prefix('brand')->group(function (){
+            Route::get('/', 'BrandController@index')->name('admin.market.brand.index');
+            Route::get('/create', 'BrandController@create')->name('admin.market.brand.create');
+            Route::post('/store', 'BrandController@store')->name('admin.market.brand.store');
+            Route::get('/edit/{id}', 'BrandController@edit')->name('admin.market.brand.edit');
+            Route::put('/update/{id}', 'BrandController@update')->name('admin.market.brand.update');
+            Route::delete('/delete/{id}', 'BrandController@destroy')->name('admin.market.brand.destroy');
+        });
+    });
 });
+
+
