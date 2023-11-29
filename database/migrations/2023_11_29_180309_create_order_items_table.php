409292eee15e63b5e3c2db5aacf068dfd1e0f802
bz4devco <alibabazadeh12y@gmail.com>
2023-11-29 21:55:32 +0330

    create migrations and database tables of orders and cart system module on store side in this website project

---
+++ database/migrations/2023_11_29_180309_create_order_items_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrderItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('order_id')->constrained('orders')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignId('product_id')->constrained('products')->onUpdate('cascade')->onDelete('cascade');
+            $table->longText('products');
+            $table->foreignId('amazing_sale_id')->nullable()->constrained('amazing_sales')->onUpdate('cascade')->onDelete('cascade');
+            $table->longText('amazing_sale_object')->nullable();
+            $table->decimal('amazing_sale_discount_amount', 20, 3)->comment('IR price unit');
+            $table->integer('number')->default(1);
+            $table->decimal('final_product_price', 20, 3);
+            $table->decimal('final_total_product_price', 20, 3)->comment('number * final_product_price');
+            $table->foreignId('color_id')->nullable()->constrained('product_colors')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignId('guarantee_id')->nullable()->constrained('guarantees')->onUpdate('cascade')->onDelete('cascade');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_items');
+    }
+}
