51f86a29b32272fba10d148a3b033373f47b0ca5
bz4devco <alibabazadeh12y@gmail.com>
2023-12-11 12:40:50 +0330

    add file manage service in this project

---
+++ app/Http/Services/File/FileService.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Services\File;
+
+
+class FileService extends FileToolsService
+{
+
+    public function moveToPublic($file)
+    {
+        //set File
+        $this->setFile($file);
+        //execute provider
+        $this->provider();
+        //save File
+        $result = $file->move(public_path($this->getFinalFileDirectory()), $this->getFinalFileName());
+        return $result ? $this->getFileAddress() : false;
+    }
+
+
+  public function moveToStorage($file)
+    {
+        //set File
+        $this->setFile($file);
+        //execute provider
+        $this->provider();
+        //save File
+        $result = $file->move(storage_path($this->getFinalFileDirectory()), $this->getFinalFileName());
+        return $result ? $this->getFileAddress() : false;
+    }
+
+
+    public function deleteFile($filePath, $storage = false)
+    {
+        if($storage){
+            unlink(storage_path($filePath));
+            return true;
+        }
+        if(file_exists($filePath))
+        {
+            unlink($filePath);
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+}
