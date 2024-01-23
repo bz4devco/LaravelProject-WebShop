ac642807356b1ff9fae84b53590c14c64c8e4456
bz4devco <alibabazadeh12y@gmail.com>
2024-01-23 10:59:29 +0330

    create 404 and 403 page for manage this errors

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html dir="rtl" lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
+    <link rel="icon" type="image/x-icon" href="{{asset('customer-assets/images/403/error-403.png')}}">
+    <title>صفحه 403 | دسترسی غیر مجاز</title>
+
+    <style>
+        @font-face {
+            font-family: "IRANSans";
+            src: url({{asset('customer-assets/fonts/IRANSans/IRANSansWeb.woff2')}});
+        }
+
+        body {
+            background-color: #ffffff;
+            margin: 0;
+            padding: 0;
+        }
+
+        body .base {
+            width: 100%;
+            height: 100vh;
+            position: relative;
+            overflow: hidden;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            cursor: pointer;
+            flex-direction: column;
+            -webkit-tap-highlight-color: rgba(255, 255, 255, 0);
+        }
+
+        body .base h1 {
+            -webkit-tap-highlight-color: rgba(255, 255, 255, 0);
+            font-family: "Ubuntu", sans-serif;
+            text-transform: uppercase;
+            text-align: center;
+            font-size: 26vw;
+            display: block;
+            margin: 0;
+            color: #9ae1e2;
+            position: relative;
+            z-index: 0;
+            animation: colors 0.4s ease-in-out forwards;
+            animation-delay: 1.7s;
+        }
+
+        body .base h1:before {
+            content: "U";
+            position: absolute;
+            top: -9%;
+            right: 40%;
+            transform: rotate(180deg);
+            font-size: 13vw;
+            color: #f6c667;
+            z-index: -1;
+            text-align: center;
+            animation: lock 0.2s ease-in-out forwards;
+            animation-delay: 1.5s;
+        }
+
+        body .base h2 {
+            font-family: "IRANSans", sans-serif;
+            color: #9ae1e2;
+            font-size: 4vw;
+            margin: 0;
+            margin-top: -40px;
+            text-transform: uppercase;
+            text-align: center;
+            animation: colors 0.4s ease-in-out forwards;
+            animation-delay: 2s;
+            -webkit-tap-highlight-color: rgba(255, 255, 255, 0);
+        }
+
+        body .base h5 {
+            font-family: "IRANSans", sans-serif;
+            color: #9ae1e2;
+            font-size: 1.7vw;
+            margin: 0;
+            text-align: center;
+            opacity: 0;
+            animation: show 2s ease-in-out forwards;
+            color: #ca3074;
+            animation-delay: 3s;
+            -webkit-tap-highlight-color: rgba(255, 255, 255, 0);
+        }
+
+        @keyframes lock {
+            50% {
+                top: -4%;
+            }
+
+            100% {
+                top: -6%;
+            }
+        }
+
+        @keyframes colors {
+            50% {
+                transform: scale(1.1);
+            }
+
+            100% {
+                color: #ca3074;
+            }
+        }
+
+        @keyframes show {
+            100% {
+                opacity: 1;
+            }
+        }
+    </style>
+</head>
+
+<body>
+    <div class="base io">
+        <h1 class="io">403</h1>
+        <h2>دسترسی غیر مجاز</h2>
+        <h5>(متاسفم دوست عزیز...)</h5>
+    </div>
+</body>
+
+</html>
---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html dir="rtl" lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="{{asset('customer-assets/images/404/969655.png')}}">
+    <link rel="stylesheet" href="{{ asset('customer-assets/css/bootstrap/bootstrap-reboot.rtl.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('customer-assets/css/bootstrap/bootstrap.rtl.min.css') }}">
+    <title>صفحه ای پیدا نشد | صفحه 404</title>
+
+    <style>
+        /*======================
+    404 page
+=======================*/
+
+        @font-face {
+            font-family: "IRANSans";
+            src: url({{asset('customer-assets/fonts/IRANSans/IRANSansWeb.woff2')}});
+        }
+
+        .page_404 {
+            padding: 40px 0 0 0;
+            background: #fff;
+            font-family: 'IRANSans', serif;
+        }
+
+        .page_404 img {
+            width: 100%;
+        }
+
+        .four_zero_four_bg {
+            background-image: url({{asset('customer-assets/images/404/dribbble_1.gif')}});
+            height: 400px;
+            background-position: center;
+        }
+
+
+        .four_zero_four_bg h1 {
+            font-size: 80px;
+        }
+
+        .four_zero_four_bg h3 {
+            font-size: 80px;
+        }
+
+        .link_404 {
+            color: #fff !important;
+            padding: 10px 20px;
+            background: #39ac31;
+            margin: 20px 0;
+            display: inline-block;
+        }
+
+        .contant_box_404 {
+            margin-top: -50px;
+        }
+    </style>
+</head>
+
+<body>
+    <section class="page_404">
+        <div class="container">
+            <div class="row">
+                <div class="col-1"></div>
+                <div class="col-10">
+                    <div class="four_zero_four_bg">
+                        <h1 class="text-center ">404</h1>
+                    </div>
+
+                    <div class="contant_box_404 text-center">
+                        <h3 class="h2">
+                            به نظر می رسد گم شده اید
+                        </h3>
+
+                        <p>صفحه ای که بدنبال آن هستید در دسترس نیست!</p>
+                        @if(request()->is('admin/*'))
+                        <a href="{{ route('admin.home') }}" class="link_404 text-decoration-none">بازگشت به صفحه قبل</a>
+                        @else
+                        <a href="{{ url()->previous() }}" class="link_404 text-decoration-none">بازگشت به صفحه قبل</a>
+                        @endif
+                    </div>
+                </div>
+            </div>
+            <div class="col-1"></div>
+        </div>
+        </div>
+    </section>
+</body>
+
+</html>
